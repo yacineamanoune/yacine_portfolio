5e0b84fbcce5ec31bb75caeb4ba3b4546f566fcc
Salim Rutaganda <rutagandasalim@gmail.com>
2025-04-03 13:42:25 +0200

    feat: add peasy.so analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Analytics />
+      <head>
+      <script src="https://cdn.peasy.so/peasy.js" data-website-id="01jp2b1pbzn6wmjsng3q8v5ax0" async></script>
+      </head>
       <body className="text-body selection:bg-primary selection:text-hoverColor mx-auto my-8 max-w-[400px] px-4 md:my-16 md:max-w-[600px]">
         <main className="font-sans">
           {children}
