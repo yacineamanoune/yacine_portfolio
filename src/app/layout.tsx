0a730f9a7086a04063ac85bf611bfbce5b411880
Salim Rutaganda <rutagandasalim@gmail.com>
2025-04-03 13:55:39 +0200

    feat: opengraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,29 @@ import { Analytics } from "@vercel/analytics/react"
 export const metadata: Metadata = {
   title: "Salim Rutaganda",
   description: "A minimal personal portfolio website made with Next.js and Tailwind CSS",
+  openGraph: {
+    title: "Salim Rutaganda",
+    description: "A minimal personal portfolio website made with Next.js and Tailwind CSS",
+    url: "https://salim.engineer",
+    siteName: "Salim Rutaganda",
+    images: [
+      {
+        url: "/og.png",
+        width: 1200,
+        height: 630,
+        alt: "Salim Rutaganda",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Salim Rutaganda",
+    description: "A minimal personal portfolio website made with Next.js and Tailwind CSS",
+    images: ["/og.png"],
+    creator: "@salimnunez01", 
+  },
 };
 
 export default function RootLayout({
