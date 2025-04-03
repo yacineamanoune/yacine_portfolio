a35b0d5dc4d8a089c96994df826333e9288124e0
Salim Rutaganda <rutagandasalim@gmail.com>
2025-04-03 17:21:03 +0200

    style: reduce bg color contrast

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
       <head>
       <script src="https://cdn.peasy.so/peasy.js" data-website-id="01jp2b1pbzn6wmjsng3q8v5ax0" async></script>
       </head>
-      <body className="text-body selection:bg-primary selection:text-hoverColor mx-auto my-8 max-w-[400px] px-4 md:my-16 md:max-w-[600px]">
+      <body className="text-body bg-[#0A0A0A] selection:bg-primary selection:text-hoverColor mx-auto my-8 max-w-[400px] px-4 md:my-16 md:max-w-[600px]">
         <main className="font-sans">
           {children}
         </main>
