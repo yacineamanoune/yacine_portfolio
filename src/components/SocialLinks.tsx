1dcee6a77275b8ba2875b5aae5d24eefdbb2d87a
Yacine <amanouneyacine@gmail.com>
2025-09-22 00:49:47 +0200

    Linkedin link

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -1,13 +1,23 @@
 import Link from 'next/link'
 import { HiOutlineDocumentText } from 'react-icons/hi'
 import { FiTwitter, FiMail } from 'react-icons/fi'
-import { FaYoutube, FaGithub  } from "react-icons/fa";
+import { FaYoutube, FaGithub,FaLinkedin } from "react-icons/fa";
 import { SiPeerlist } from "react-icons/si";
 
 
 export default function SocialLinks() {
   return (
     <div className="w-fit grid grid-cols-3 gap-4 pt-4 tracking-tight md:flex md:flex-row md:items-start">
+
+      <Link
+          href="https://www.linkedin.com/in/yacine-amanoune/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="group flex items-center underline-offset-4 transition duration-150 ease-in-out md:hover:text-accent md:hover:underline"
+        >
+          <FaLinkedin className="mr-1 h-4 w-4" />
+          <span className="text-sm text-primary">linkedin</span>
+        </Link>
 
       <Link
         href="https://x.com/yacamanou"
